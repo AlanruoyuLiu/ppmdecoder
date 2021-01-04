124af5fbd4d366cebe2370fcea69d96a011a90a7
Ruoyu <1357390966@qq.com>
2021-01-04 21:19:12 +0800

    revise sof_received.v

--- clk16.v
+++ clk16.v
@@ -7,7 +7,11 @@ module clk16 (
     reg [3:0] cnt_16;
     wire clk_16_out;
 
-assign clk_16_out = (cnt_16 > 4'd7)? 1'b1 : 1'b0;
+    reg first, sec;
+
+    wire clk16_old;
+
+assign clk16_old = (cnt_16 > 4'd7)? 1'b1 : 1'b0;
 /* this is a dff, so there is a clk delay
     always @(posedge clk or negedge rst_n) begin
        if (!rst_n) begin
@@ -24,4 +28,27 @@ assign clk_16_out = (cnt_16 > 4'd7)? 1'b1 : 1'b0;
             cnt_16 <= cnt_16 + 4'b0001;
         end 
     end
+
+// The first DFF
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            first <= 1'b0;
+        end else begin
+            first <= clk16_old;
+        end
+    end
+/*
+// The second DFF
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            sec <= 1'b0;
+        end else begin
+            sec <= first;
+        end
+    end
+*/
+// clc
+assign clk_16_out = ~first && clk16_old;
+
+
 endmodule
--- run.do
+++ run.do
@@ -7,6 +7,11 @@ vopt +acc tb_decoderPPM -o tb_decoderPPM_opt
 vsim tb_decoderPPM_opt
 add wave -position insertpoint sim:/tb_decoderPPM/DUT/*
 
+
+add wave -noupdate tb_decoderPPM/DUT/u1/clk16_old
+add wave -noupdate tb_decoderPPM/DUT/u1/first
+add wave -noupdate tb_decoderPPM/DUT/u1/cnt_16
+
 add wave -noupdate tb_decoderPPM/DUT/u2/cnt_2bits
 add wave -noupdate tb_decoderPPM/DUT/u2/shift_00
 add wave -noupdate tb_decoderPPM/DUT/u2/shift_01
@@ -22,6 +27,7 @@ add wave -noupdate tb_decoderPPM/DUT/u3/onebyte_out
 
 add wave -noupdate tb_decoderPPM/DUT/u5/cnt_sof
 add wave -noupdate tb_decoderPPM/DUT/u5/start
+add wave -noupdate tb_decoderPPM/DUT/u5/eof_rcv_in
 add wave -noupdate tb_decoderPPM/DUT/u7/cnt_eof
 
 
